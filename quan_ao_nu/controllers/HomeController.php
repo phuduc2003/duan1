e1a69ac878e427c3076ed8e9574b184c42686964
phuduc2003 <ducphu2400@gmail.com>
2024-11-17 13:42:36 +0700

    Hien thi chi tiet san pham

--- quan_ao_nu/controllers/HomeController.php
+++ quan_ao_nu/controllers/HomeController.php
@@ -18,4 +18,22 @@ class HomeController
       echo "Có lỗi xảy ra: " . $e->getMessage();
     }
   }  
+  public function detailSanPham()
+    {
+        $id = $_GET['id_san_pham'];
+        // var_dump($id);die();
+        $sanPham = $this->modelSanPham->getDetailSanPham($id);
+        $listAnhSanPham = $this->modelSanPham->getListAnhSanPham($id);
+        $listBinhLuan = $this->modelSanPham->getBinhLuanFromSanPham($id);
+        $listSanPhamCungDanhMuc = $this->modelSanPham->getListSanPhamDanhMuc($sanPham['danh_muc_id']);
+        //  var_dump($listSanPhamCungDanhMuc);die();
+        if ($sanPham) {
+            require_once "./views/chiTietSanPham.php";
+        
+        } else {
+            header("Location: " . BASE_URL );
+            exit();
+        }
+
+    }
 }
